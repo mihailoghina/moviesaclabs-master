5990353b518b716031b8a9ec6ddf6e25b0958d06
mihailoghina <mihailoghina@gmail.com>
2016-04-11 16:52:18 +0300

    lab3SV
    remove functional

---
+++ MoviesACLabs/Scripts/jslab3.js
@@ -0,0 +1,26 @@
+﻿var poz = 1;
+var curr_id;
+
+function addmovie() {
+    curr_id = "id" + poz;
+    var a = $("#link").val();
+    $("ul").append('<li ' + curr_id + '>' + a + '<button type="button" id="removebutton'+poz+'">x</button></li>');
+
+    
+    var rmv1 = $("#removebutton" + poz);
+    rmv1.click(scoateFilm);
+
+    poz++;
+
+};
+
+var btn = $("#addbutton");
+btn.click(addmovie);
+
+
+
+function scoateFilm() {
+    $(this).parent().remove();
+
+}
+
