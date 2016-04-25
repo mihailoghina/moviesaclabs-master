2b0da5a7fe2853bcf00c5d45f4307be6d36311f9
mihailoghina <mihailoghina@gmail.com>
2016-04-25 16:23:48 +0300

    lab ANGULAR JS CONTINUED
    ADD SI FILTER

---
+++ MoviesACLabs/Scripts/angularjscont.js
@@ -0,0 +1,49 @@
+﻿var app = angular.module("movies", []);
+
+app.service("MoviesService", function ($http) {
+    this.getMovies = function () {
+        return $http({
+            method: "GET",
+            url: "/api/movies"
+        });
+    },
+
+    this.adaugaFilm = function (movie) {
+        return $http({
+            method: "POST",
+            url: "/api/movies",
+            data: movie
+        });     
+    },
+
+    this.stergeFilm = function (stergefilm) {
+        return $http({
+            method: "DELETE",
+            url: "/api/movies",
+            data: stergefilm
+        });
+    }
+});
+
+app.controller('MoviesCtrl', function ($scope, MoviesService) {
+    
+    $scope.moviesList = null;
+
+    MoviesService.getMovies().then(function (dataResponse) {
+        $scope.moviesList = dataResponse;
+    });
+
+    $scope.movie = {};
+    $scope.movie.Title = "";
+    $scope.movie.Description = "";
+
+    $scope.stergefilm = "";
+
+    $scope.addClick = function () {
+        MoviesService.adaugaFilm($scope.movie);
+    }
+
+    $scope.deleteClick = function () {
+        MoviesService.stergeFilm($scope.stergefilm);
+    }
+});
--- MoviesACLabs/Scripts/jslab3.js
+++ MoviesACLabs/Scripts/jslab3.js
@@ -5,7 +5,6 @@ function addmovie() {
     curr_id = "id" + poz;
     var a = $("#link").val();
     $("ul").append('<li ' + curr_id + '>' + a + '<button type="button" id="removebutton'+poz+'">x</button></li>');
-
     
     var rmv1 = $("#removebutton" + poz);
     rmv1.click(scoateFilm);
